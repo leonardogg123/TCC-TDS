4b8b35c4eac5bab6933b3bfbd8afc600c2cedae1
leonardogg123 <leonardo.gabrig@escola.pr.gov.br>
2023-10-22 19:29:29 -0300

    TCC LEO & MIH

---
+++ view/components/container.php
@@ -0,0 +1,18 @@
+
+
+<div class="container">
+<div class="item">
+        <img src="img/logo_viagens.png" alt="Seu logotipo" class="logo"><h2 class="logo">Poupe Travel</h2>    
+            <div class="text-item"><br>
+                <h2>Bem-Vindo!</h2><br>
+                <p>Seja bem vindo ao Poupe Travel! estamos aqui para ajudar você a organizar a melhor viagem possível! </p>
+                <div class="social-icon">
+                    <a href="https://www.facebook.com"><i class='bx bxl-facebook'></i></a>
+                    <a href="https://twitter.com"><i class='bx bxl-twitter'></i></a>
+                    <a href="https://www.youtube.com/"><i class='bx bxl-youtube'></i></a>
+                    <a href="https://www.instagram.com"><i class='bx bxl-instagram'></i></a>
+                    <a href="https://www.linkedin.com"><i class='bx bxl-linkedin'></i></a>
+                </div>
+            </div>
+        </div>
+</div>
